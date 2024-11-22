5e07e7c2fda5254b8987b14dbe97d70f3a3be630
Anton Lazurko <antla@ciklum.com>
2024-11-22 19:24:48 +0200

    feat: add request parsing for form data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import fs from 'fs';
+import queryString from 'querystring';
 
 import mainRouter from './routes/index.js';
 
@@ -13,6 +14,22 @@ const logStream = fs.createWriteStream(path.join('log', 'app.log'), { flags: 'a'
 app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  if (req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+    let data = '';
+    req.on('data', (chunk) => (data += chunk.toString()));
+    req.on('end', () => {
+      const parsedFormData = queryString.parse(data);
+      console.log(parsedFormData);
+      req.body = parsedFormData;
+      next();
+    });
+  } else {
+    console.log(req.body);
+    next();
+  }
+});
 
 app.use('/', mainRouter);
 
