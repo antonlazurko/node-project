4a214b4983e4add2dd5ad832d39136aa110131dd
Anton Lazurko <antla@ciklum.com>
2024-11-22 18:00:02 +0200

    feat: add log middleware

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,14 +1,11 @@
 export const getUsersHandler = (req, res) => {
-  console.log('Hello get users!');
   res.send('Hello get users!');
 };
 
 export const getUserByIdHandler = (req, res) => {
-  console.log('Hello get user by id!');
   res.send('Hello get user by id!');
 };
 
 export const postUsersHandler = (req, res) => {
-  console.log('Hello post users!');
   res.send('Hello post users!');
 };
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import fs from 'fs';
+
 import rootRouter from './routes/index.js';
 
 const app = express();
+
+const logStream = fs.createWriteStream(path.join('log', 'app.log'), { flags: 'a' });
+
+app.use(morgan('combined', { stream: logStream }));
 
 app.use('/', rootRouter);
 
